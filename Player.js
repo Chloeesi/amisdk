199e7d89f3457dca56f484d785aa409a548cc38a
Chloe Arguin <34430936+Chloeesi@users.noreply.github.com>
2017-12-10 20:33:42 -1000

    Update Player.js

--- Player.js
+++ Player.js
@@ -19,3 +19,54 @@ var Player = function(mediaElement) {
 
   this.setupCallbacks_();
 };
+/**
+ * Attaches necessary callbacks.
+ * @private
+ */
+Player.prototype.setupCallbacks_ = function() {
+  var self = this;
+
+  // Google Cast device is disconnected from sender app.
+  this.castReceiverManager_.onSenderDisconnected = function() {
+    window.close();
+  };
+
+  // Receives messages from sender app. The message is a comma separated string
+  // where the first substring indicates the function to be called and the
+  // following substrings are the parameters to be passed to the function.
+  this.imaMessageBus_.onMessage = function(event) {
+    console.log(event.data);
+    var message = event.data.split(',');
+    var method = message[0];
+    switch (method) {
+      case 'requestAd':
+        var adTag = message[1];
+        var currentTime = parseFloat(message[2]);
+        self.requestAd_(adTag, currentTime);
+        break;
+      case 'seek':
+        var time = parseFloat(message[1]);
+        self.seek_(time);
+        break;
+      default:
+        self.broadcast_('Message not recognized');
+        break;
+    }
+  };
+
+  // Initializes IMA SDK when Media Manager is loaded.
+  this.mediaManager_.onLoad = function(event) {
+    self.originalOnLoadEvent_ = event;
+    self.initIMA_();
+    self.originalOnLoad_(self.originalOnLoadEvent_);
+  };
+};
+
+/**
+ * Sends messages to all connected sender apps.
+ * @param {!string} message Message to be sent to senders.
+ * @private
+ */
+Player.prototype.broadcast_ = function(message) {
+  this.imaMessageBus_.broadcast(message);
+};
